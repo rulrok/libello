df20c0a1df89001dc3ebe7f697eed36c54dfbab6
reuel <reuel@lab_cead_p077529.unifal-mg.edu.br>
2013-09-27 10:17:03 -0300

    Arquivo de configurações

--- biblioteca/configuracoes.php
+++ biblioteca/configuracoes.php
@@ -1,23 +1,28 @@
 <?php
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 if (!defined("APP_CONFIGS")) {
     define("APP_CONFIGS", "SETTED");
 
     define("ROOT", $_SERVER['DOCUMENT_ROOT'] . "/controle-cead/");
-    define("APP_LOCATION", ROOT."app/");
+    define("APP_LOCATION", ROOT . "app/");
 
-    define("WEB_SERVER_NAME", "http://localhost/controle-cead/");
+    define("WEB_SERVER_FOLDER","controle-cead/");
+    define("WEB_SERVER_ADDRESS", "http://172.16.11.1:8888/".WEB_SERVER_FOLDER);
 
     define("DATABASE_SERVER_IP", "127.0.0.1");
     define("DATABASE_SERVER_DBNAME", "novo_controle_cead");
     define("DATABASE_SERVER_USER", "root");
-    define("DATABASE_SERVER_PASSWORD", "");
-    
+    define("DATABASE_SERVER_PASSWORD", "root");
+
     define("SMTP_SERVER_IP", "200.131.224.99");
     define("SMTP_SERVER_PORT", "587");
     define("SMTP_SERVER_PASSWORD", "CeAd-N000");
     define("SMTP_SERVER_EMAIL", "cead-noreply@unifal-mg.edu.br");
-    
-    define("BIBLIOTECA_DIR",ROOT."biblioteca/");
+
+    define("BIBLIOTECA_DIR", ROOT . "biblioteca/");
+
+    define("SECRET", md5("controleCEAD@2013"));
 }
 ?>
