537910638798f8de70f653a4b75978a2345283e8
alberto <alberto@alberto-Aspire-4739>
2014-06-23 11:17:21 -0300

    Regressão de commit ao commit:  "Reparo na remoção de cursos e polos, bem como..." (0902bfb)

--- app/modelo/ferramentas/cursospolos/removercurso.php
+++ app/modelo/ferramentas/cursospolos/removercurso.php
@@ -3,7 +3,7 @@
 $id = (int) fnDecrypt(filter_input(INPUT_GET, 'cursoID'));
 $mensagem = new Mensagem();
 if ((new cursoDAO())->remover($id)) {
-//    (new sistemaDAO())->registrarExclusaoCurso(obterUsuarioSessao()->get_idUsuario());
+    (new sistemaDAO())->registrarExclusaoCurso(obterUsuarioSessao()->get_idUsuario());
     $mensagem->set_mensagemSucesso("Excluído com sucesso.");
 } else {
     $mensagem->set_mensagemErro("Erro ao excluir");
