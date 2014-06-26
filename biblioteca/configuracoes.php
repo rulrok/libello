648c5d761b89a7dfdc05b7e05a904d48ac1cce1b
alberto <alberto@alberto-Aspire-4739>
2014-06-26 09:53:12 -0300

    Modificação do arquivo "configurações"

--- biblioteca/configuracoes.php
+++ biblioteca/configuracoes.php
@@ -22,7 +22,7 @@ if (!defined("APP_CONFIGS")) {
     /**
      * O novo da pasta de contém este programa instalado
      */
-    define('WEB_SERVER_FOLDER', "controle-cead");
+    define('WEB_SERVER_FOLDER', "libello");
     /**
      * Endereço pelo qual o acesso deverá ser feito ao programa
      */
@@ -32,7 +32,7 @@ if (!defined("APP_CONFIGS")) {
     define('DATABASE_SERVER_PORT', "3306");
     define('DATABASE_SERVER_DBNAME', "libello");
     define('DATABASE_SERVER_USER', "root");
-    define('DATABASE_SERVER_PASSWORD', "1261");
+    define('DATABASE_SERVER_PASSWORD', "YOUR_PASSWORD");
 
     define('SMTP_SERVER_IP', "200.131.224.99");
     define('SMTP_SERVER_PORT', "587");
